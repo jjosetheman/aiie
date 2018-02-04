2cad37c84569173320e18d043a19f464a72e18bd
Jorj Bauer <jorj@jorj.org>
2018-02-04 12:45:03 -0500

    new pin abstraction for Teensy

---
+++ teensy/pins.h
@@ -0,0 +1,47 @@
+#ifndef __PINS_H
+#define __PINS_H
+
+// 0 is serial1
+// 1 is serial1
+#define RAM_D0 2
+//3
+//4
+#define RAM_D7 5
+#define RAM_D4 6
+#define RAM_D2 7
+#define RAM_D3 8
+#define RAM_A3 9
+#define RAM_A4 10
+#define RAM_A6 11
+#define RAM_A7 12
+#define RAM_A5 13
+#define RAM_D1 14
+#define RAM_A0 15
+#define RAM_A12 16
+#define RAM_A13 17
+#define RAM_A15 18
+#define RAM_A14 19
+#define RAM_D5 20
+#define RAM_D6 21
+#define RAM_A1 22
+#define RAM_A2 23
+// ...
+#define BATTERYPIN 32 // aka A13
+// 33
+#define RAM_RW 34
+#define RAM_A8 35
+#define RAM_A9 36
+#define RAM_A10 37
+#define RAM_A11 38
+#define RESET 39
+// ...
+
+#define RAM_A16 49
+#define RAM_A17 50
+
+#define PADDLE0 A10
+#define PADDLE1 A11
+#define SPEAKERPIN A21 // aka DAC0
+// A22 (DAC1)
+
+#endif
